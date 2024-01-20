74ec3cb3ff799674a3e881c292bcfdd0814c0084
Sanjida <64713109+Cantmakeit@users.noreply.github.com>
2024-01-20 20:57:35 +0600

    euler.m

--- euler.m
+++ euler.m
@@ -12,13 +12,15 @@ y(1) = 0.5;
 h = (b-a)/(n*1.0);
 
 for i = 1:n;
-    y(i+1) = y(i) + h * f( a + (i-1)*h , y(i));
+    ti = a + (i-1)*h;
+    y(i+1) = y(i) + h * f( ti , y(i));
 end
 
 syms t
 exact_sol = (t + 1)^2 - 0.5 * exp(t);
 
 for i = 1 : n+1
-    exact = subs(exact_sol, a + (i-1)*h);
+     ti = a + (i-1)*h;
+    exact = subs(exact_sol, ti);
     fprintf("t: %0.2f \ty: %0.7f\t exact: %0.7f \t%0.9f \n", a+(i-1)*h, y(i), exact, abs(y(i)-exact));
 end
